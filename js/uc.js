79c4fa17f93b82b17ac513a5f2c6ced3a3c6afe9
Agudanieldev <162652813+Agudanieldev@users.noreply.github.com>
2024-03-09 09:51:53 +0100

    Add files via upload

---
+++ js/uc.js
@@ -0,0 +1,28 @@
+
+
+let UserCreds = JSON.parse(sessionStorage.getItem("user-creds"));
+let UserInfo = JSON.parse(sessionStorage.getItem("user-info"));
+
+let MsgHead = document.getElementById('msg');
+let GreetHead = document.getElementById('greet');
+let MainBalance = document.getElementById('mainbalance');
+let SignoutBtn = document.getElementById('signoutbutton');
+
+let Signout = () => {
+  sessionStorage.removeItem("user-creds"); 
+  sessionStorage.removeItem("user-info");  
+  window.location.href = "../login.html";
+}
+
+let Checkcred = () => {
+  if (!sessionStorage.getItem("user-creds"))
+    window.location.href = "../login.html";
+  else {
+    MsgHead.innerText = `${UserCreds.email}`;
+    GreetHead.innerText = `Welcome! ${UserInfo.firstname}`;
+    MainBalance.innerText = `${UserInfo.balance}`;
+  }
+}
+
+window.addEventListener('load', Checkcred);
+SignoutBtn.addEventListener('click', Signout);
