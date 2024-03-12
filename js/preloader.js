e16db38cd911236a26d55311113c595f1d1cf727
Agudanieldev <162652813+Agudanieldev@users.noreply.github.com>
2024-03-12 11:43:33 +0100

    Add files via upload

---
+++ js/preloader.js
@@ -0,0 +1,27 @@
+(function($) {
+	
+	"use strict";
+	
+	//Hide Loading Box (Preloader)
+	function handlePreloader() {
+		if($('.loader-wrap').length){
+			$('.loader-wrap').delay(1000).fadeOut(500);
+		}
+	}
+
+	if ($(".preloader-close").length) {
+        $(".preloader-close").on("click", function(){
+            $('.loader-wrap').delay(200).fadeOut(500);
+        })
+    }
+	
+	
+	
+	$(window).on('load', function() {
+		handlePreloader();
+		enableMasonry();
+	});
+
+	
+
+})(window.jQuery);
