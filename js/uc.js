facffc8c1e2f4f94996e45ffd11741455d06cab5
Agudaniel <adofintl@gmail.com>
2025-05-27 04:20:24 +0100

    adjusted-userCreds

--- js/uc.js
+++ js/uc.js
@@ -9,17 +9,21 @@ let SignoutBtn = document.getElementById('signoutbutton');
 
 let ActiveDeposit = document.getElementById('activedeposit');
 let Profit = document.getElementById('profit');
+let InvestmentTier = document.getElementById('investmentTier');
+let TotalPropertiesOwned = document.getElementById('totalPropertiesOwned');
 let TotalBalance = document.getElementById('totalbalance');
 
 let Checkcred = () => {
   if (!sessionStorage.getItem("user-creds"))
     window.location.href = "index.html";
   else {
-    MsgHead.innerText = `${UserCreds.email}`;
+    MsgHead.innerText = UserCreds.email;
     GreetHead.innerText = `Welcome! ${UserInfo.firstname}`;
-    ActiveDeposit.innerText = `${UserInfo.active_deposit}`;
-    Profit.innerText = `${UserInfo.profit}`;
-    TotalBalance.innerText = `${UserInfo.total_balance}`;
+    ActiveDeposit.innerText = UserInfo.active_deposit;
+    Profit.innerText = UserInfo.profit;
+    InvestmentTier.innerText = UserInfo.investment_tier;
+    TotalPropertiesOwned.innerText = UserInfo.total_properties_owned;
+    TotalBalance.innerText = UserInfo.total_balance;
   }
 }
 window.addEventListener('load', Checkcred);
